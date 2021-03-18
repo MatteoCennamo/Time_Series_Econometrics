530760d61299e926e5fbb69e8517314354ae22ed
MatteoCennamo <74864960+MatteoCennamo@users.noreply.github.com>
2021-03-18 23:29:06 +0100

    Update Project.R

--- Project.R
+++ Project.R
@@ -1,14 +1,10 @@
 library(ggplot2)
+library(gridExtra)
+library(MSGARCH)
 
 load("~/R/Time_Seties_Econometrics/DataForPresentation2021_timeseries.RData")
 View(StockReturns)
 StockReturns[, 1] <- as.Date(StockReturns[, 1], format = "%d/%m/%Y")
-
-#Starting from 02/01/1991
-Stock1991 <- StockReturns[StockReturns$Date > as.Date("1990-12-31"), ]
-
-
-
 
 # Multiple plot of the returns
 cnames <- colnames(StockReturns)
@@ -32,16 +28,77 @@ do.call(grid.arrange, c(plotList, nrow = 2))
 # Assumption of 0 mean is confirmed
 
 
-# Create a time series
-class(StockReturns) # "data.frame"
-df <- ts(StockReturns[, -1], frequency = 252)
-rownames(df) <- as.Date(StockReturns[, 1], format = "%d/%m/%Y")
-plot(df)
-
-# Conditional variance dynamics: ARCH, sGARCH, eGARCH, gjrGARCH, tGARCH
+# Conditional variance dynamics: sARCH, sGARCH, eGARCH, gjrGARCH, tGARCH
 # Conditional distributions: norm, std (t-student), ged, snorm, sstd, sged
 
 # 1) CreateSpec -> Create a model specification
 # 2) FitML      -> Fit the model by maximum likelihood
 # 3) predict    -> Compute the conditional volatility forecasts and the density forecasts
 # 4) Risk       -> Compute the value-at-risk and expected-shortfall risk measures
+
+# 1) MODEL SPECIFICATION
+# Specifies 2 states:
+var_dynamic   <- c('eGARCH', 'eGARCH')
+distributions <- c('std', 'std')
+spec <- CreateSpec(variance.spec = list(model = var_dynamic),      # variance dynamic
+                   distribution.spec = list(distribution = distributions), # distributions
+                   switch.spec = list(do.mix = F, # a MSGARCH is modeled (default)
+                                      K = NULL),  # n of components is decided by the len of variance.spec
+                   constraint.spec = list(fixed = NULL,        # fixed parameters set by the user
+                                          regime.const = NULL) # parameters set equal across regimes
+                   ) 
+# The relevant information is summarized with the summary method:
+summary(spec)
+
+# 2) MODEL ESTIMATION
+# Using maximum likelihood:
+data <- StockReturns[, 2]
+model <- FitML(spec, data,     # specifications and data
+               ctr = list(     # control parameters (different from FitMCMC())
+                 par0 = NULL,  # starting parameters that overwrite the default starting parameter scheme
+                 do.se = T,    # if standard errors should be computed (yes = T, no = F)
+                 do.plm = F    # F = optimization step is performed without ensuring stationarity for the volatility processes
+#                 OptimFUN =   # Custom optimization function (default: Broyden-Fletcher-Goldfarb-Shanno)
+               ))
+summary(model)
+# All estimated parameters are significant at 95% confidence level.
+# State 2 is the one with higher average volatility (higher 'nu').
+# State 1 and 2 do not differ so much with respect to the response of previous values ('beta' is the same)
+# [State 2 is a bit more responsive, as can be expected].
+# Both the states are very consistent (high value on the main diagonal of the transiction matrix).
+# Belonging to state 2 is a bit more probable (54.54%).
+# LL: -8362.5148  -> Log-liklihood
+# AIC: 16749.0297 -> Akaike information criterion
+# BIC: 16830.2805 -> Bayesian information criterion
+
+smooth.prob <- State(model)$SmoothProb[, 1, 2, drop = TRUE]
+ggplot(StockReturns, aes(x = Date)) + 
+  geom_line(aes(y = data), color = 'black', size = 0.4) + 
+  geom_line(aes(y = smooth.prob[-1] * 10), color = 'red', size = 0.9) + 
+  theme_minimal() + 
+  scale_x_date(date_minor_breaks = '1 year') + 
+  scale_y_continuous(name = 'Returns', 
+                     sec.axis = sec_axis(~./10, name = 'State'))
+
+# 3) FORECASTING
+# We can forcas: (i) shape of the distribution of yT+h|IT, (ii) volatility.
+predicted_days = 30L
+pred <- predict(model, 
+  nahead = predicted_days, # Horizon of the prediction
+  ctr = list(nsim = 1e4L)) # Number of simulations(*)
+# (*) For h = 1, the predictive volatility and density are available in closed form; 
+# in other circumstances (if h > 1) forecasts are obtained by simulation, drawing 
+# iteratively a new observation from the (one-step ahead) predictive distribution 
+# and updating the K conditional variance processes accordingly.
+
+# The predict() method returns an object of class ‘MSGARCH_FORECAST’ with two elements:
+#     vol  -> numeric vector of length h, containing the standard deviations of the distributions 
+#             yT+j|IT for j = 1, ..., h.
+#     draw -> matrix of dimension h × nsim of the simulated MSGARCH process.
+
+# Predicted volatility for the next 30 days:
+print(pred$vol)
+
+
+# 4) IVE RISK MANAGEMENT
+
