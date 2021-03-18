a85a7e7532a4b367bf4e02a9613670bc57a8671f
MatteoCennamo <74864960+MatteoCennamo@users.noreply.github.com>
2021-03-18 17:25:13 +0100

    Update Project.R

--- Project.R
+++ Project.R
@@ -10,17 +10,27 @@ Stock1991 <- StockReturns[StockReturns$Date > as.Date("1990-12-31"), ]
 
 
 
-ggplot(data = StockReturns, aes(x = Date, y = SP500)) +
-  geom_line() +
-  labs(x = "Date",
-       y = "SP500",
-       title = "Returns",
-       subtitle = "from 26/11/1990 to 22/06/2016") + 
-  scale_x_date(date_breaks = '5 year', 
-               date_minor_breaks = '1 year', date_labels = '%Y', 
-               limits = c(as.Date('01/01/1990', format = "%d/%m/%Y"), NA))
+# Multiple plot of the returns
+cnames <- colnames(StockReturns)
+plotList <- list()
+for (i in seq(2, length(cnames))){
+  if (i %in% c(2, 5)){
+    ylab = "Returns"
+  } else {
+    ylab = ""
+  }
+  plotList[[i - 1]] <- ggplot(data = StockReturns, aes(x = Date, y = StockReturns[, i])) +
+    geom_line() + 
+    labs(x = '', title = cnames[i], y = ylab) + 
+    scale_x_date(date_breaks = '10 year', 
+                 date_minor_breaks = '1 year', date_labels = '%Y', 
+                 limits = c(as.Date('01/01/1989', format = "%d/%m/%Y"), 
+                            as.Date('01/01/2019', format = "%d/%m/%Y")))
+}
+do.call(grid.arrange, c(plotList, nrow = 2))
 
 
+# Create the time series
 class(StockReturns) # "data.frame"
 df <- ts(StockReturns[, -1], frequency = 252)
 rownames(df) <- as.Date(StockReturns[, 1], format = "%d/%m/%Y")
